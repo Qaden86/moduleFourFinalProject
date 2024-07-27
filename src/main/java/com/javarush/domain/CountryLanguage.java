2c02d07a72650a713246d0652c8bba1337b1bca4
bp-dteplo504 <qaden86@gmail.com>
2024-07-27 21:24:26 +0300

    Initial commit

---
+++ src/main/java/com/javarush/domain/CountryLanguage.java
@@ -0,0 +1,34 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(schema = "world", name = "country_language")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CountryLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String language;
+
+    @Column(name = "is_official", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isOfficial;
+
+    private BigDecimal percentage;
+
+}
